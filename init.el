3781aa662015dd590b162945073923c6979e7523
Vladimir Mihailenco <vladimir.webdev@gmail.com>
2022-04-24 10:11:41 +0300

    Upd

--- init.el
+++ init.el
@@ -1,17 +1,21 @@
-(setq gc-cons-threshold 20000000)
+(setq gc-cons-threshold 100000000)
 
 (setq inhibit-splash-screen t)
 (setq-default tab-width 3)
-(setq auto-save-default nil)
 (set-frame-font "Source Code Pro Medium-11")
+
+;; don't create lock files
+(setq create-lockfiles nil)
+;;(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
 
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 (setq initial-major-mode 'text-mode)
 (setq initial-scratch-message nil)
 (add-hook 'dired-mode-hook 'dired-hide-details-mode)
+(electric-pair-mode 1)
 
-(setq python-shell-interpreter "python3.6")
+(setq python-shell-interpreter "python3.8")
 (defvaralias 'flycheck-python-pylint-executable 'python-shell-interpreter)
 
 (setq js-indent-level 2)
@@ -39,8 +43,8 @@
   (when (not (string= (file-name-extension buffer-file-name) "http"))
     (delete-trailing-whitespace)))
 
-(setq-default whitespace-style '(face trailing lines empty indentation::space))
-(setq-default whitespace-line-column 80)
+(setq-default whitespace-style '(face trailing lines empty))
+(setq-default whitespace-line-column 100)
 (add-hook 'before-save-hook 'my-delete-trailing-whitespace)
 (global-whitespace-mode t)
 
@@ -55,102 +59,117 @@
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
-(package-install 'use-package))
+  (package-install 'use-package))
+
+(unless (package-installed-p 'diminish)
+  (package-refresh-contents)
+  (package-install 'diminish))
+
+(eval-when-compile
+  (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
 
 ;-------------------------------------------------------------------------------
 
-(use-package better-defaults
-  :ensure t)
+(use-package flyspell
+  :defer t
+  :init
+  (progn
+    (add-hook 'prog-mode-hook 'flyspell-prog-mode)
+    (add-hook 'text-mode-hook 'flyspell-mode)
+    )
+  :config
+  (setq-default ispell-program-name "hunspell")
+  (setq ispell-really-hunspell t)
+  (setq ispell-local-dictionary-alist
+        `((nil "[[:alpha:]]" "[^[:alpha:]]" "[']" t ("-d" "en_US") nil utf-8)))
+  )
+
+(use-package flyspell-correct
+  :after flyspell
+  :bind (:map flyspell-mode-map ("C-;" . flyspell-correct-wrapper)))
+
+(use-package flyspell-correct-ivy
+  :after flyspell-correct)
+
+;-------------------------------------------------------------------------------
+
+(use-package ivy
+  :defer t
+  :diminish
+  :config
+  (ivy-mode))
+
+(use-package counsel
+  :after ivy
+  :diminish
+  :config (counsel-mode))
+
+(use-package ivy-rich
+  :hook (counsel-mode . ivy-rich-mode))
+
+(use-package swiper
+  :after ivy
+  :bind (("C-s" . swiper)
+         ("C-r" . swiper)))
+
+;-------------------------------------------------------------------------------
+
+(use-package better-defaults)
 
 
 (use-package which-key
-  :ensure t
+  :diminish
   :config
   (which-key-mode))
 
 
-(use-package base16-theme
-  :ensure t
-  :config (load-theme 'base16-eighties t))
-
-
-(use-package smartparens
-  :ensure t
-  :init
-  (use-package smartparens-config)
-  (use-package smartparens-python)
-  (use-package smartparens-html)
-  (use-package smartparens-markdown)
-  (use-package smartparens-javascript)
-  (smartparens-global-mode 1)
-  (show-smartparens-global-mode 1)
-  :config
-  (setq smartparens-strict-mode t))
+ (use-package base16-theme
+   :config
+   (load-theme 'base16-eighties t))
 
 
 (use-package editorconfig
-  :ensure t
+  :diminish
   :config
   (editorconfig-mode 1))
 
 
 (use-package highlight-symbol
-  :ensure t
   :config
   (setq highlight-symbol-idle-delay 0.5)
   (add-hook 'prog-mode-hook 'highlight-symbol-mode))
 
 
 (use-package ag
-  :ensure t
   :defer t
   :commands (ag ag-regexp ag-project))
 
 
-(use-package smex
-  :ensure t
-  :config
-  (global-set-key (kbd "M-x") 'smex))
-
-
-(use-package flx-ido
-  :ensure t
-  :init
-  (flx-ido-mode 1)
-  (setq ido-enable-flex-matching t)
-  (setq ido-use-faces nil))
-
-
-(use-package ido-completing-read+
-  :ensure t
-  :config
-  (ido-ubiquitous-mode 1))
-
-
 (use-package projectile
-  :ensure t
+  :diminish
   :config
   (projectile-mode)
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
 
 
 (use-package magit
-  :ensure t
   :defer 3
   :bind
-  ("C-x g" . magit-status)
-  :config
-  (setq magit-completing-read-function 'magit-ido-completing-read))
+  ("C-x g" . magit-status))
 
 
 (use-package flycheck
-  :ensure t
   :defer 3
-  :config (global-flycheck-mode 1))
+  :config
+  (global-flycheck-mode 1))
 
 
 (use-package markdown-mode
-  :ensure t
   :commands
   (markdown-mode gfm-mode)
   :mode
@@ -162,33 +181,32 @@
 
 
 (use-package yaml-mode
-  :ensure t
   :mode ("\\.ya?ml\\'" . yaml-mode))
 
 
+(use-package kotlin-mode
+  :mode ("\\.kts\\'" . kotlin-mode))
+
+
 (use-package expand-region
-  :ensure t
   :defer t
   :bind ("C-=" . er/expand-region))
 
 
 (use-package go-mode
-  :ensure t
   :mode ("\\.go\\'" . go-mode)
   :init
-  (setq gofmt-command "goimports")
+  (setq gofmt-command "gofumports")
   :config
   (add-hook 'before-save-hook 'gofmt-before-save))
 
 
 (use-package flycheck-golangci-lint
-  :ensure t
   :hook (go-mode . flycheck-golangci-lint-setup)
   :init)
 
 
 (use-package coffee-mode
-  :ensure t
   :mode
   ("\\.coffee\\'" . coffee-mode)
   :init
@@ -196,7 +214,6 @@
 
 
 (use-package typescript-mode
-  :ensure t
   :mode
   ("\\.ts\\'" . typescript-mode)
   :init
@@ -204,33 +221,27 @@
 
 
 (use-package web-mode
-  :ensure t
   :mode
   ("\\.erb\\'" . web-mode)
   ("\\.mustache\\'" . web-mode)
-  ("\\.html?\\'" . web-mode)
-  :config
-  (progn
-    (setq web-mode-markup-indent-offset 2
-          web-mode-css-indent-offset 2
-          web-mode-code-indent-offset 2)))
+  ("\\.html\\'" . web-mode)
+  ("\\.vue\\'" . web-mode)
+  ("\\.mjml\\'" . web-mode)
+  :custom
+  (web-mode-script-padding 0)
+  (web-mode-style-padding 0)
+  (web-mode-block-padding 0)
+  (web-mode-markup-indent-offset 2)
+  (web-mode-css-indent-offset 2)
+  (web-mode-code-indent-offset 2))
 
+(use-package lua-mode
+  :mode
+  ("\\.lua\\'" . lua-mode))
 
 (use-package php-mode
-  :ensure t
   :mode
   ("\\.php\\'" . php-mode))
-
-
-(use-package mmm-mode
-  :ensure t
-  :init
-  (setq mmm-js-mode-enter-hook (lambda () (setq syntax-ppss-table nil)))
-  (setq mmm-typescript-mode-enter-hook (lambda () (setq syntax-ppss-table nil))))
-
-
-(use-package vue-html-mode
-  :ensure t)
 
 
 (use-package css-mode
@@ -238,18 +249,37 @@
   (setq css-indent-offset 2))
 
 
-(use-package edit-indirect
-  :ensure t)
+(use-package rubocopfmt
+  :hook
+  (ruby-mode . rubocopfmt-mode)
+  :custom
+  (rubocopfmt-use-bundler-when-possible nil))
+
+(use-package edit-indirect)
 
 
-(use-package vue-mode
-  :ensure t
-  :mode
-  ("\\.vue\\'" . vue-mode))
+(use-package yaml-mode
+  :mode ("\\.ya?ml\\'" . yaml-mode))
+
+
+(use-package csharp-mode
+  :mode ("\\.cs\\'" . csharp-mode))
+
+
+;; (use-package vue-mode
+;;   :ensure t
+;;   :mode
+;;   ("\\.vue\\'" . vue-mode))
+
+
+;; (use-package mmm-mode
+;;   :ensure t
+;;   :init
+;;   (setq mmm-js-mode-enter-hook (lambda () (setq syntax-ppss-table nil)))
+;;   (setq mmm-typescript-mode-enter-hook (lambda () (setq syntax-ppss-table nil))))
 
 
 (use-package pug-mode
-  :ensure t
   :mode
   ("\\.pug\\'" . pug-mode)
   :init
@@ -257,63 +287,74 @@
 
 
 (use-package scss-mode
-  :ensure t
   :mode
   ("\\.scss\\'" . scss-mode))
 
+
 (use-package prettier-js
-  :ensure t
   :config
+  (add-hook 'xml-hook 'prettier-js-mode)
+  (add-hook 'web-mode-hook 'prettier-js-mode)
   (add-hook 'js-mode-hook 'prettier-js-mode)
+  (add-hook 'java-mode-hook 'prettier-js-mode)
   (add-hook 'vue-mode-hook 'prettier-js-mode)
   (add-hook 'typescript-mode-hook 'prettier-js-mode)
   (add-hook 'scss-mode-hook 'prettier-js-mode)
-  (add-hook 'xml-hook 'prettier-js-mode)
-  :init
-  (setq prettier-js-args '("--trailing-comma" "all")))
+  (add-hook 'markdown-mode-hook 'prettier-js-mode)
+  (add-hook 'yaml-mode-hook 'prettier-js-mode))
+
 
 (use-package blacken
-  :ensure t
   :config
   (add-hook 'python-mode-hook 'blacken-mode))
 
+(use-package protobuf-mode
+  :mode ("\\.proto\\'" . protobuf-mode))
+
+(use-package langtool
+  :init
+  (setq langtool-language-tool-jar "/home/vmihailenco/Downloads/LanguageTool-5.1/languagetool-commandline.jar"))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package lsp-mode
-  :ensure t
-  :commands (lsp lsp-deferred)
-  :hook (go-mode . lsp-deferred))
+;; (use-package lsp-mode
+;;   :ensure t
+;;   :config
+;;   (setq lsp-enable-completion-at-point nil)
+;;   :commands
+;;   (lsp lsp-deferred)
+;;   :hook
+;;   (go-mode . lsp-deferred))
 
 ;; Set up before-save hooks to format buffer and add/delete imports.
 ;; Make sure you don't have other gofmt/goimports hooks enabled.
-(defun lsp-go-install-save-hooks ()
-  (add-hook 'before-save-hook #'lsp-format-buffer t t)
-  (add-hook 'before-save-hook #'lsp-organize-imports t t))
-(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+;(defun lsp-go-install-save-hooks ()
+;  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+;  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+;(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
 
-;; Optional - provides fancier overlays.
-(use-package lsp-ui
-  :ensure t
-  :commands lsp-ui-mode)
+;; ;; Optional - provides fancier overlays.
+;; (use-package lsp-ui
+;;   :ensure t
+;;   :commands lsp-ui-mode)
 
-;; Company mode is a standard completion package that works well with lsp-mode.
-(use-package company
-  :ensure t
-  :config
-  ;; Optionally enable completion-as-you-type behavior.
-  (setq company-idle-delay 0)
-  (setq company-minimum-prefix-length 1))
+;; ;; Company mode is a standard completion package that works well with lsp-mode.
+;; (use-package company
+;;   :ensure t
+;;   :config
+;;   ;; Optionally enable completion-as-you-type behavior.
+;;   (setq company-idle-delay 0)
+;;   (setq company-minimum-prefix-length 1))
 
-;; company-lsp integrates company mode completion with lsp-mode.
-;; completion-at-point also works out of the box but doesn't support snippets.
-(use-package company-lsp
-  :ensure t
-  :commands company-lsp)
+;; ;; company-lsp integrates company mode completion with lsp-mode.
+;; ;; completion-at-point also works out of the box but doesn't support snippets.
+;; (use-package company-lsp
+;;   :ensure t
+;;   :commands company-lsp)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(use-package hydra
-  :ensure t)
+(use-package hydra)
 
 (defhydra hydra-lsp (:exit t :hint nil)
   "
@@ -375,5 +416,3 @@
   (setq meghanada-javac-xlint "-Xlint:all,-processing")
   :commands
   (meghanada-mode))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
